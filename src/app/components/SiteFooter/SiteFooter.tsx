431657e19f5a1cb7acce7597fd9e1d99aab5fe6f
hansied67 <hansied678@gmail.com>
2025-03-21 18:29:42 -0500

    Footer Tailwind changes

--- src/app/components/SiteFooter/SiteFooter.tsx
+++ src/app/components/SiteFooter/SiteFooter.tsx
@@ -1,42 +1,57 @@
 'use client';
 
+import Image from "next/image";
 import Link from "next/link";
 
 
 export default function SiteFooter() {
     return(
         <footer className="fixed w-full bottom-0 border-t border-gray-200 dark:border-gray-600 items-center justify-center p-1">
-          <div className="text-center">Made with&nbsp;
-            <Link
-            className="text-blue-600 hover:text-gray-400 visited:text-purple-600"
-            href="https://nextjs.org/"
-            target="_blank"
-            rel="noopener noreferrer">
-                Next.js
-            </Link>
-            &nbsp;using&nbsp;
-            <Link
-            className="text-blue-600 hover:text-[#61DBFB] visited:text-purple-600"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer">
-                Tailwind CSS
-            </Link>
-            <br />
-            Assets made with&nbsp;
-            <Link
-            className="text-blue-500 hover:text-cyan-100 visited:text-purple-600"
-            href="https://www.aseprite.org/"
-            target="_blank">
-                Aseprite
-            </Link>
-            &nbsp;by&nbsp;
-            <Link
-            className="text-blue-600 hover:text-green-500 visited:text-purple-600"
-            href="https://www.linkedin.com/in/hdurchholz/"
-            target="_blank">
-                Hans Durchholz :)
-            </Link>
+          <div className="max-w-screen-xl flex items-center justify-between mx-auto">
+            <div className="flex flex-1" />
+            <div className="flex flex-2 justify-center">
+              <div className="text-center justify-items-center">Made with&nbsp;
+                  <Link
+                  className="text-blue-600 hover:text-gray-400 visited:text-purple-600"
+                  href="https://nextjs.org/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                      Next.js
+                  </Link>
+                  &nbsp;using&nbsp;
+                  <Link
+                  className="text-blue-600 hover:text-[#61DBFB] visited:text-purple-600"
+                  href="https://tailwindcss.com/"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                      Tailwind CSS
+                  </Link>
+                  <br />
+                  Assets made with&nbsp;
+                  <Link
+                  className="text-blue-500 hover:text-cyan-100 visited:text-purple-600"
+                  href="https://www.aseprite.org/"
+                  target="_blank">
+                      Aseprite
+                  </Link>
+                  &nbsp;by&nbsp;
+                  <Link
+                  className="text-blue-600 hover:text-green-500 visited:text-purple-600"
+                  href="https://www.linkedin.com/in/hdurchholz/"
+                  target="_blank">
+                      Hans Durchholz :)
+                  </Link>
+              </div>
+            </div>
+            <div className="flex flex-1 justify-end">
+                <Image
+                aria-hidden
+                src="/worm_foreground.png"
+                alt="Worm 3"
+                width={64}
+                height={64}
+                />
+            </div>
           </div>
         </footer>
     )
