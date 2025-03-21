6527139d91df2f8b7ddeb2eb0bf67c98d6ac18c9
hansied67 <hansied678@gmail.com>
2025-03-21 01:01:10 -0500

    Custom ThemeToggle Component

---
+++ src/app/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+
+//TODO: Change sprite on click. Easy implementation, make new worm sprite?
+export default function ThemeToggle() {
+    const [theme, setTheme] = useState(false);
+
+    useEffect(() => {
+        setTheme(window.matchMedia("(prefers-color-scheme:dark)").matches)
+    }, [])
+
+    function toggleTheme() {
+        if (theme) {
+            document.documentElement.style.setProperty('--background', '#ededed')
+            document.documentElement.style.setProperty('--foreground', '#0a0a0a')
+        } else {
+            document.documentElement.style.setProperty('--background', '#0a0a0a')
+            document.documentElement.style.setProperty('--foreground', '#ededed')
+        }
+        setTheme(!theme);
+        console.log(theme);
+    }
+
+    return (
+        <button className="" onClick={toggleTheme}>
+            <Image
+            className="opacity-80 hover:opacity-100"
+            src="/worm_foreground.png"
+            alt="Dark/Light mode toggle"
+            width={64}
+            height={64}
+            priority
+            />
+        </button>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,79 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import ThemeToggle from "./components/ThemeToggle/ThemeToggle";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="light grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <ThemeToggle />
         <h1 className="text-5xl dark:text-violet-800">
           Wanna see a frog?
         </h1>
         <Image
-          className=""
-          src="/Frog.gif"
-          alt="64x64 frog pixel art gif made in Aseprite"
-          width={256}
-          height={256}
-          priority
+        className=""
+        src="/Frog.gif"
+        alt="64x64 frog pixel art gif made in Aseprite"
+        width={256}
+        height={256}
+        unoptimized
         />
       </main>
       <footer className="row-start-3 items-center justify-center">
         <div className="flex gap-[24px] flex-wrap justify-center">
           <Image
-            aria-hidden
-            src="/worm_foreground.png"
-            alt="Worm 1"
-            width={64}
-            height={64}
+          aria-hidden
+          src="/worm_foreground.png"
+          alt="Worm 1"
+          width={64}
+          height={64}
             />
           <Image
-            aria-hidden
-            src="/flower.png"
-            alt="flower"
-            width={64}
-            height={64}
+          aria-hidden
+          src="/flower.png"
+          alt="flower"
+          width={64}
+          height={64}
           />
           <Image
-            aria-hidden
-            src="/worm_foreground.png"
-            alt="Worm 3"
-            width={64}
-            height={64}
+          aria-hidden
+          src="/worm_foreground.png"
+          alt="Worm 3"
+          width={64}
+          height={64}
           />
         </div>
         <div className="text-center">Made with&nbsp;
           <Link
-            className="text-blue-600 hover:bg-text:blue-600 visited:text-purple-600"
-            href="https://nextjs.org/"
-            target="_blank"
-            rel="noopener noreferrer">
-              Next.js
+          className="text-blue-600 hover:bg-text:blue-600 visited:text-purple-600"
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noopener noreferrer">
+            Next.js
           </Link>
           &nbsp;using&nbsp;
           <Link
-            className="text-blue-600 hover:text-[#61DBFB] visited:text-purple-600"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer">
-              Tailwind CSS
+          className="text-blue-600 hover:text-[#61DBFB] visited:text-purple-600"
+          href="https://tailwindcss.com/"
+          target="_blank"
+          rel="noopener noreferrer">
+            Tailwind CSS
           </Link>
           <br />
           Assets made with&nbsp;
           <Link
-            className="text-blue-500 hover:text-green-500 visited:text-purple-600"
-            href="https://www.aseprite.org/"
-            target="_blank">
-              Aseprite
+          className="text-blue-500 hover:text-green-500 visited:text-purple-600"
+          href="https://www.aseprite.org/"
+          target="_blank">
+            Aseprite
           </Link>
           &nbsp;by&nbsp;
           <Link
-            className="text-blue-600 hover:text-green-500 visited:text-purple-600"
-            href="https://www.linkedin.com/in/hdurchholz/"
-            target="_blank">
-              Hans Durchholz :)
+          className="text-blue-600 hover:text-green-500 visited:text-purple-600"
+          href="https://www.linkedin.com/in/hdurchholz/"
+          target="_blank">
+            Hans Durchholz :)
           </Link>
         </div>
       </footer>
