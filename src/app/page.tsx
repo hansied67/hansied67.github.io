e55b1db864f678ed1594992e988efc3ac2d3f906
hansied67 <hansied678@gmail.com>
2025-03-20 23:43:07 -0500

    Changed basic configs, playing with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,78 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <h1 className="text-5xl dark:text-violet-800">
+          Wanna see a frog?
+        </h1>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className=""
+          src="/Frog.gif"
+          alt="64x64 frog pixel art gif made in Aseprite"
+          width={256}
+          height={256}
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <footer className="row-start-3 items-center justify-center">
+        <div className="flex gap-[24px] flex-wrap justify-center">
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+            src="/worm_foreground.png"
+            alt="Worm 1"
+            width={64}
+            height={64}
+            />
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/flower.png"
+            alt="flower"
+            width={64}
+            height={64}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src="/worm_foreground.png"
+            alt="Worm 3"
+            width={64}
+            height={64}
           />
-          Go to nextjs.org →
-        </a>
+        </div>
+        <div className="text-center">Made with&nbsp;
+          <Link
+            className="text-blue-600 hover:bg-text:blue-600 visited:text-purple-600"
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer">
+              Next.js
+          </Link>
+          &nbsp;using&nbsp;
+          <Link
+            className="text-blue-600 hover:text-[#61DBFB] visited:text-purple-600"
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer">
+              Tailwind CSS
+          </Link>
+          <br />
+          Assets made with&nbsp;
+          <Link
+            className="text-blue-500 hover:text-green-500 visited:text-purple-600"
+            href="https://www.aseprite.org/"
+            target="_blank">
+              Aseprite
+          </Link>
+          &nbsp;by&nbsp;
+          <Link
+            className="text-blue-600 hover:text-green-500 visited:text-purple-600"
+            href="https://www.linkedin.com/in/hdurchholz/"
+            target="_blank">
+              Hans Durchholz :)
+          </Link>
+        </div>
       </footer>
     </div>
   );
