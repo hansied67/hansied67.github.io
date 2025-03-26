26aef8d56646cba252bf60a4957c2f343d9eade1
hansied67 <hansied678@gmail.com>
2025-03-25 20:55:04 -0500

    Portfolio: Assets, Tailwind for CustomCarousel Component
    This is more of a headache than I expected!!

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,14 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
 import CustomCarousel from "../components/CustomCarousel/CustomCarousel";
 
 export default function Portfolio() {
     return (
         <div className="light grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <main className="flex flex-col gap-[32px] row-start-2">
-            <CustomCarousel>
-              <Image src="/worm_foreground.png" alt="worm :)" width={64} height={64} className="h-full w-full object-cover"/>
-              <Image src="/worm_foreground.png" alt="worm :)" width={64} height={64} className="h-full w-full object-cover"/>
-            </CustomCarousel>
+            <div className="flex flex-row gap-[32px] border-2 rounded-xl p-2">
+              <div className="flex flex-3 py-2 pl-2">
+                <h1 className="font-bold underline text-6xl">CTRBot</h1>
+                <h2 className="italic text-lg self-center flex">
+                  Interface for
+                  <Link
+                    className="text-blue-500 hover:text-green-500 visited:text-purple-500"
+                    href="https://crashteamranking.com/nftttable/"
+                    target="_blank">
+                      &nbsp;Crash Team Ranking
+                  </Link>
+                  &apos;s back-end.
+                </h2>
+              </div>
+              <CustomCarousel className="flex flex-1 border-1 rounded-lg">
+                <Image src="/CTRBot SC1.png" alt="" width={600} height={0} className="h-full w-full m-auto"/>
+                <Image src="/CTRBot SC2.png" alt="" width={600} height={0} className="h-full w-full m-auto"/>
+              </CustomCarousel>
+            </div>
           </main>
         </div>
     );
