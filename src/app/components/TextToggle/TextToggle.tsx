4eceb549b2067750cd95a2b04cda512572b6b449
hansied67 <hansied678@gmail.com>
2025-04-03 21:24:27 -0500

    localStorage for theme preferences

--- src/app/components/TextToggle/TextToggle.tsx
+++ src/app/components/TextToggle/TextToggle.tsx
@@ -1,19 +1,46 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function TextToggle(props: { className: string | undefined; }) {
     const [isPixelFont, setPixelFont] = useState(true);
+
+    useEffect(() => {
+        const preference = window?.localStorage.getItem('font-preference');
+        const mainContainer = document.getElementById("main-container");
+        if (preference === "null") {
+            mainContainer?.classList.remove("font-sans");
+            mainContainer?.classList.add("font-pixel");
+            window.localStorage.setItem('font-preference', 'pixel');
+            setPixelFont(true);
+        }
+        else {
+            if (preference == "pixel") {
+                mainContainer?.classList.remove("font-sans");
+                mainContainer?.classList.add("font-pixel");
+                window.localStorage.setItem('font-preference', 'pixel');
+                setPixelFont(true);  
+            }
+            else {
+                mainContainer?.classList.remove("font-pixel");
+                mainContainer?.classList.add("font-sans");
+                window.localStorage.setItem('font-preference', 'sans');
+                setPixelFont(false);  
+            }
+        }
+    }, [])
 
     function toggleFont() {
         const mainContainer = document.getElementById("main-container");
         if (isPixelFont) {
             mainContainer?.classList.remove("font-pixel");
             mainContainer?.classList.add("font-sans");
+            window.localStorage.setItem('font-preference', 'sans');
         }
         else {
             mainContainer?.classList.remove("font-sans");
             mainContainer?.classList.add("font-pixel");
+            window.localStorage.setItem('font-preference', 'pixel');
         }
         setPixelFont(!isPixelFont);
     }
--- src/app/components/ThemeToggle/ThemeToggle.tsx
+++ src/app/components/ThemeToggle/ThemeToggle.tsx
@@ -6,22 +6,52 @@ import Image from "next/image";
 
 //TODO: Change sprite on click. Easy implementation, make new worm sprite?
 export default function ThemeToggle(props: { className: string | undefined; }) {
-    const [isDark, setTheme] = useState(false);
+    const [isDark, setTheme] = useState(true);
 
     useEffect(() => {
-        setTheme(window.matchMedia("(prefers-color-scheme:dark)").matches)
+        const preference = window?.localStorage.getItem('theme-preference');
+        console.log("useEffect: preference: " + preference);
+        const root = document.querySelector<HTMLElement>(":root");
+
+        if (preference === "null") {
+          const browserPreference = window.matchMedia("(prefers-color-scheme:dark)").matches;
+          root?.style.setProperty('--background', '#0a0a0a')
+          root?.style.setProperty('--foreground', '#ededed')
+          root?.style.setProperty('--background-image', `url("/site-bg-dark.png")`)
+          window.localStorage.setItem('theme-preference', (browserPreference ? 'dark': 'light'))
+          setTheme(browserPreference)
+        }
+        else {
+            if (preference == "dark") {
+              root?.style.setProperty('--background', '#0a0a0a')
+              root?.style.setProperty('--foreground', '#ededed')
+              root?.style.setProperty('--background-image', `url("/site-bg-dark.png")`)
+              window.localStorage.setItem('theme-preference', 'dark');
+              setTheme(true);
+            }
+            else {
+              root?.style.setProperty('--background', '#ededed')
+              root?.style.setProperty('--foreground', '#0a0a0a')
+              root?.style.setProperty('--background-image', `url("/site-bg-light.png")`)
+              window.localStorage.setItem('theme-preference', 'light');
+              setTheme(false);
+            }
+        }
+        console.log(preference);
     }, [])
 
     function toggleTheme() {
-      const root = document.querySelector<HTMLElement>(":root");
+        const root = document.querySelector<HTMLElement>(":root");
         if (isDark) {
             root?.style.setProperty('--background', '#ededed')
             root?.style.setProperty('--foreground', '#0a0a0a')
             root?.style.setProperty('--background-image', `url("/site-bg-light.png")`)
+            window.localStorage.setItem('theme-preference', 'light');
         } else {
             root?.style.setProperty('--background', '#0a0a0a')
             root?.style.setProperty('--foreground', '#ededed')
             root?.style.setProperty('--background-image', `url("/site-bg-dark.png")`)
+            window.localStorage.setItem('theme-preference', 'dark');
         }
         setTheme(!isDark);
     }
