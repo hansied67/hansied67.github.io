16648b8e529304d4d6727cef54ec9fa4b19cfdf5
hansied67 <hansied678@gmail.com>
2025-04-07 23:26:07 -0500

    Alt/Title text updates per W3CAG 2.1
    Additional modifications to portolio entry descriptions

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -23,14 +23,27 @@ export default function Portfolio() {
               href="https://flutter.dev/"
               target="_blank">
                 Flutter
-              </Link>.
+              </Link>
+              &nbsp;&amp;&nbsp;
+              <Link
+              href="https://dart.dev/"
+              target="_blank">
+                Dart
+              </Link>
+              , styled with&nbsp;
+              <Link
+              href="https://m3.material.io/"
+              target="_blank">
+                Google&apos;s Material Design
+              </Link>
+              .
               <br /><br />
             </span>}
             description={
             <span>
               This was an app I made for a friend! They were dissatisfied with the customization available in other popular free dice roller apps.
               <br />
-              The app serves as a standard dice rolling app with a &quot;Custom Roller&quot; tab for specific, spells, actions, etc. I had a great time designing the app&apos;s sleek, professional Material design and custom assets. Seeing the fruits of my labor with my own eyes in a casual setting was very fulfilling! One of my favorite pet projects for this reason!
+              The app serves as a standard dice rolling app with a &quot;Custom Roller&quot; tab for specific, spells, actions, etc. I had a great time designing the app&apos;s sleek Material design and custom assets. Seeing the fruits of my labor with my own eyes in a casual setting was very fulfilling! One of my favorite pet projects for this reason!
               <br />
               This was my first foray into mobile app development. It was available on Google Play Store for over 5 years; the site has since replaced the mobile app. It still runs perfectly on Android/iOS/web - the app publishing was more of a formality and a learning experience, anyway.
               <br />
@@ -56,8 +69,8 @@ export default function Portfolio() {
             >
               <Image
               src="/DiceRoller1.gif"
-              title="D&D Dice Roller showcase"
-              alt="D&D app gif"
+              title="Dice Roller tab showcase - Footage of a user rolls 1 of each standard die type, re-rolling a few times, and clearing the output."
+              alt="D&D Dice Roller app - Dice Roller tab gif"
               width={250}
               height={0}
               sizes="100vw"
@@ -66,8 +79,8 @@ export default function Portfolio() {
               />
               <Image
               src="/DiceRoller2.gif"
-              title="D&D Dice Roller: Custom Roller showcase"
-              alt="D&D app custom roller gif"
+              title="Custom Roller showcase - Footage of the user using the Custom Roller, acting as 'macros' for dice roll and modifier combinations."
+              alt="D&D Dice Roller app - Custom Roller gif"
               width={250}
               height={0}
               sizes="100vw"
@@ -76,8 +89,8 @@ export default function Portfolio() {
               />
               <Image
               src="/DiceRoller3.png"
-              title="D&D Dice Roller: History showcase "
-              alt="D&D app history"
+              title="History showcase - Screenshot of the app's history interface, displaying the rolls enacted by the user in prior gifs."
+              alt="D&D Dice Roller app - Roll History"
               width={250}
               height={0}
               sizes="100vw"
@@ -113,7 +126,7 @@ export default function Portfolio() {
             <span>
               This bot was a collaborative project with my friends to be used by other friends! We spun up a Python bot, made an API to hook-up to the site&apos;s back-end, and hosted the server on Linux.
               <br />
-              CTRBot exists to streamline the Crash Team Ranking user experience by replacing the site&apos;s UI functionalities with command-based ones. This provides efficiency, reliability, and the ability to perform several site functions concurrently. Furthermore, this bot contains custom functionality such as track generation and custom track pools, to further act as a general-use competitive tool.
+              CTRBot exists to streamline the Crash Team Ranking user experience by replacing the site&apos;s UI functionalities with command-based ones, as well as adding custom filtering options. This provides efficiency, reliability, and the ability to perform several site functions concurrently. Furthermore, this bot contains custom functionality such as track generation and custom track pools, to further act as a general-use competitive tool.
               <br />
               This bot was beta tested and utilized by hundreds of members of competitive CTR communities within discord. I led the development team of 2 other developers, teaching them Git, Python, Linux Operations, JSON, the works!
               <br />
@@ -121,7 +134,7 @@ export default function Portfolio() {
             </span>}>
               <Image
               src="/CTRBot SC1.png"
-              title="A screenshot of a Discord Chat window between a user and CTRBot, showcasing track generation adhering to custom pools. The user inputs '.p bs'. The bot outputs an image of 8 tracks with their names using CTR's font, stacked horizontally."
+              title="A screenshot of a Discord Chat window between a user and CTRBot, showcasing track generation. The bot outputs an image of a set amount of tracks with their names using CTR's font, stacked horizontally. The amount of tracks and the pool of tracks to choose from are determined by the user's input."
               alt="Discord Screenshot - CTRBot track command"
               width={400}
               height={0}
@@ -130,7 +143,7 @@ export default function Portfolio() {
               />
               <Image
               src="/CTRBot SC2.png"
-              title="A screenshot of a Discord Chat window between a user and CTRBot, showcasing the bot's aggregation and display of top 10 player times per track. The user inputs '.lap10 inferno', and the bot outputs an embedded message detailing the 10 fastest lap times for the track Inferno Island. The embedded message contains a hyperlink to the track's leaderboard on CrashTeamRanking.com, a hyperlink to CrashTeamRanking.com's home page via its icon, and 10 embedded messages for each player's time. Said embeds contain: position, username, country flag, engine type (icon), lap time hyperlinked to its Youtube URL, platform (icon), and a link to the user's CrashTeamRanking profile."
+              title="A screenshot of a Discord Chat window between a user and CTRBot, showcasing the bot's aggregation and display of top track times submitted to CrashTeamRanking.com. The bot outputs a neatly formatted embedded message containing all the information and hyperlinks present on that track's leaderboard."
               alt="Discord Screenshot - CTRBot record commands"
               width={400}
               height={0}
@@ -143,7 +156,37 @@ export default function Portfolio() {
             title="Glass Bridge Memory Game"
             subtitle={
             <span>
-              A Progressive Web Application coded in ReactJS, CSS, and a flask back-end, hosted via heroku (now out of commission). This was a cross-platform game based on the popular Netflix series
+              A Progressive Web Application coded in&nbsp;
+              <Link
+              href="https://react.dev/"
+              target="_blank">
+                ReactJS
+              </Link>
+              &nbsp;&amp;&nbsp;
+              <Link
+              href="https://www.w3.org/Style/CSS/Overview.en.html"
+              target="_blank">
+                CSS
+              </Link>
+              &nbsp;with a&nbsp;
+              <Link
+              href="https://flask.palletsprojects.com/en/stable/"
+              target="_blank">
+                Flask
+              </Link>
+              &nbsp;back-end, hosted via&nbsp;
+              <Link
+              href="https://react.dev/"
+              target="_blank">
+                Heroku
+              </Link>
+              &nbsp;(site no longer active). This was a cross-platform game based on the popular&nbsp;
+              <Link
+              href="https://www.netflix.com/"
+              target="_blank">
+                Netflix
+              </Link>
+              &nbsp;series
               <Link
               href="https://www.netflix.com/title/81040344"
               target="_blank">
@@ -154,13 +197,15 @@ export default function Portfolio() {
             </span>}
             description={
             <span>
-              As the developer with the most experience making responsive web apps, I took the lead on the UI team for my Senior Capstone project. We were prompted to make a game for small children to be played at a Girl Scouts retreat. Squid Game was huge at the time, so we based our game and its aesthetic on the show to draw players in.
+              I took the lead on this Senior Capstone project, and was able to utilize my prior app development knowledge to great effect.
+              <br />
+              We were prompted to make a game for small children to be played at a Girl Scouts retreat. Squid Game was huge at the time, so we based our game and its aesthetic on the show to draw players in.
               <br />
               I created the front-end&apos;s styles, components, and signals, complete with persistent state management, hooks to our Flask &quot;High Scores&quot; back-end, and custom assets.
               <br />
               I also set up a manifest and service workers in order for our game to be a Progressive Web App. This allowed our users to pin the site to their home screens like a normal app, and play offline with local high scores.
               <br />
-              Finally, I managed all the bells and whistles pertinent to our scope, including internal/external metadata, documentation, responsive design, video production for remote presentation, etc.
+              Finally, I managed all the bells and whistles pertinent to our scope, including internal/external metadata, documentation, responsive design, and video production for remote presentation.
             </span>}>
               <Image
               src="/GlassBridge.png"
