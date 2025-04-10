01e2f4c34b88cf57b60133f09bc060058a3bcd8d
hansied67 <hansied678@gmail.com>
2025-04-10 07:00:58 -0500

    robots.txt is not valid per PageSpeed Insights

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,13 +3,12 @@ import type { MetadataRoute } from 'next'
 export const dynamic = 'force-static'
  
 export default function robots(): MetadataRoute.Robots {
-  const domain = process.env.NEXT_PUBLIC_APP_DOMAIN
   return {
     rules: {
       userAgent: '*',
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: `${domain}/sitemap.xml`,
+    sitemap: 'https://hansdurchholz.com/sitemap.xml',
   }
 }
