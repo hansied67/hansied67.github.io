0ded7b778f1c1626aaa8b362076c8d313080ea35
hansied67 <hansied678@gmail.com>
2025-04-11 14:46:42 -0500

    Add new resume path to sitemap for better indexing(?)
    I 'temporarily removed' the old path on Google Search Console earlier today and that worked (the page was 404'ing). Hopefully this indexes the new one better.
    Might not be necessary - might just need to wait

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -14,16 +14,22 @@ export default function sitemap(): MetadataRoute.Sitemap {
         {
             url: "https://hansdurchholz.com/bio",
             lastModified: new Date(),
-            changeFrequency: 'monthly',
+            changeFrequency: 'yearly',
             priority: 0.7,
             images: ["https://hansdurchholz.com/HeidiHans.jpg"]
         },
         {
             url: "https://hansdurchholz.com/portfolio",
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8,
+            changeFrequency: 'monthly',
+            priority: 0.9,
             images: ['https://hansdurchholz.com/PortfolioSC.png', 'https://hansdurchholz.com/PROS%20logo.png', 'https://hansdurchholz.com/GlassBridgeLoginDialogue.png', 'https://hansdurchholz.com/DiceRollerImage.png', 'https://hansdurchholz.com/CTRBot%20SC1.png'],
         },
+        {
+            url: "https://hansdurchholz.com/docs/HansDurchholz-Resume.pdf",
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.8
+        }
     ]
 }
