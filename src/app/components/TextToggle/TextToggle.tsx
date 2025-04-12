408218150424cdc9477cf7957ccd533d21cea0c7
hansied67 <hansied678@gmail.com>
2025-04-11 23:09:16 -0500

    bug fix
    font-prompt was being set on second load

--- src/app/components/TextToggle/TextToggle.tsx
+++ src/app/components/TextToggle/TextToggle.tsx
@@ -8,28 +8,30 @@ export default function TextToggle(props: { className: string | undefined; }) {
         const fontPreference = window?.localStorage.getItem("font-preference");
         const mainContainer = document.getElementById("main-container");
 
-        console.log("font-preference: " + fontPreference);
-        console.log("font-prompt: " + window.localStorage.getItem("font-prompt"))
-
         if (fontPreference === null) {
             mainContainer?.classList.remove("font-sans");
             mainContainer?.classList.add("font-pixel");
             window.localStorage.setItem("font-preference", "pixel");
         }
         else {
+            const fontPrompt = window.localStorage.getItem("font-prompt");
             if (fontPreference == "pixel") {
                 mainContainer?.classList.remove("font-sans");
                 mainContainer?.classList.add("font-pixel");
 
-                window.localStorage.setItem("font-prompt", "No");
                 window.localStorage.setItem("font-preference", "pixel");
+                if (fontPrompt !== null) {
+                    window.localStorage.setItem("font-prompt", "No")
+                };
             }
             else {
                 mainContainer?.classList.remove("font-pixel");
                 mainContainer?.classList.add("font-sans");
 
-                window.localStorage.setItem("font-prompt", "Yes");
                 window.localStorage.setItem("font-preference", "sans");
+                if (fontPrompt !== null) {
+                    window.localStorage.setItem("font-prompt", "Yes");
+                }
             }
         }
     }, [])
@@ -42,15 +44,15 @@ export default function TextToggle(props: { className: string | undefined; }) {
             mainContainer?.classList.remove("font-pixel");
             mainContainer?.classList.add("font-sans");
 
-            window.localStorage.setItem("font-preference", "sans");
             window.localStorage.setItem("font-prompt", "Yes");
+            window.localStorage.setItem("font-preference", "sans");
         }
         else {
             mainContainer?.classList.remove("font-sans");
             mainContainer?.classList.add("font-pixel");
 
-            window.localStorage.setItem("font-preference", "pixel");
             window.localStorage.setItem("font-prompt", "No");
+            window.localStorage.setItem("font-preference", "pixel");
         }
     }
 
