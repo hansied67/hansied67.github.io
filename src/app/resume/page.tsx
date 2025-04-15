7cadcdba3942091ac014adba2f8686a59bc8acd4
hansied67 <hansied678@gmail.com>
2025-04-15 16:20:22 -0500

    URGENT - site isn't showing up on google anymore!!! :(

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { permanentRedirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Hans Durchholz - Resume",
@@ -32,6 +32,6 @@ export const metadata: Metadata = {
 
 export default function Resume() {
   return (
-    permanentRedirect('/docs/HansDurchholz-Resume.pdf')
+    redirect('/docs/HansDurchholz-Resume.pdf')
   )
 }
