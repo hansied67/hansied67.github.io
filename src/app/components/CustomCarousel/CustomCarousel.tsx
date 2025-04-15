47aaffb6dab78f5a388f30394f0ec1ba75469f26
hansied67 <hansied678@gmail.com>
2025-04-15 02:16:43 -0500

    more custom cursor stuff

--- src/app/components/CustomCarousel/CustomCarousel.tsx
+++ src/app/components/CustomCarousel/CustomCarousel.tsx
@@ -30,7 +30,7 @@ export default function CustomCarousel(props: CarouselProps) {
               size="lg"
               aria-label={carouselTitle + " Back"}
               onClick={handlePrev}
-              className="!absolute top-2/4 left-2 -translate-y-2/4 cursor-pointer"
+              className="!absolute top-2/4 left-2 -translate-y-2/4"
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
@@ -44,7 +44,7 @@ export default function CustomCarousel(props: CarouselProps) {
               size="lg"
               aria-label={carouselTitle + " Next"}
               onClick={handleNext}
-              className="!absolute top-2/4 !right-2 -translate-y-2/4 cursor-pointer"
+              className="!absolute top-2/4 !right-2 -translate-y-2/4"
             >
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
@@ -54,7 +54,7 @@ export default function CustomCarousel(props: CarouselProps) {
           navigation={({ setActiveIndex, activeIndex, length }) => (
             <div role="tablist" aria-label="Choose slide to display" className="absolute bottom-2 left-2/4 z-50 flex -translate-x-2/4 gap-2">
               {new Array(length).fill("").map((_, i) => (
-                <span
+                <div
                   key={i}
                   role="tab"
                   aria-label={carouselTitle + " Jump to Slide " + (i+1).toString()}
