185c1dddf4489d46bb8c6b77db3d4e6d41654547
hansied67 <hansied678@gmail.com>
2025-04-16 00:50:05 -0500

    More responsive CSS

--- src/app/components/SiteFooter/SiteFooter.tsx
+++ src/app/components/SiteFooter/SiteFooter.tsx
@@ -3,14 +3,47 @@
 import Link from "next/link";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import TextToggle from "../TextToggle/TextToggle";
-
+// import { useState, useEffect } from "react";
 
 export default function SiteFooter() {
+    // gave up on this, I preferred the re-styled footer.
+    // android devices with bottom bars like Pixel 7s are too much of a pain
+    //
+    // const [isScrollDown, setScrollDown] = useState(false);
+    // const [isScrollDelay, setScrollDelay] = useState(false);
+    // const [scrollY, setScrollY] = useState(0);
+
+    // useEffect(() => {
+    //   const handleScroll = () => {
+    //     const currentScrollY = window.scrollY
+
+    //     if (currentScrollY > scrollY) {
+    //       setScrollDown(true);
+    //       setTimeout(() => {
+    //         setScrollDelay(true);
+    //       }, 250)
+    //     }
+    //     else {
+    //       setScrollDown(false);
+    //       setScrollDelay(false);
+    //     }
+    //     setScrollY(currentScrollY);
+    //   }
+
+    //   if (typeof window !== 'undefined') {
+    //     window.addEventListener('scroll', handleScroll);
+
+    //     // cleanup
+    //     return () => window.removeEventListener('scroll', handleScroll);
+    //   }
+    // }, [scrollY]);
+  
+
     return(
         <footer aria-label="Site Footer with Theme Toggles" className="fixed w-full bottom-0 items-stretch backdrop-blur">
           <div className="max-w-screen-xl flex mx-auto">
             <ThemeToggle className="flex flex-2 justify-end"/>
-            <div role="complementary" aria-label="Site Info" className="flex flex-6 justify-center flex-col text-xs lg:text-base">
+            <div role="complementary" aria-label="Site Info" className="flex flex-13 justify-center flex-col text-xs md:text-base">
               <p className="text-center justify-items-center">Coded with&nbsp;
                 <Link
                 href="https://nextjs.org/"
@@ -35,12 +68,12 @@ export default function SiteFooter() {
                 target="_blank">
                   Aseprite
                 </Link>
-                . ©&nbsp;
+                .&nbsp;
                 <Link
                 href="https://github.com/hansied67/hansied67.github.io/blob/main/LICENSE"
                 aria-label="Site Github License"
                 target="_blank">
-                  2025
+                  ©&nbsp;2025
                 </Link>
                 &nbsp;
                 <Link
@@ -61,7 +94,7 @@ export default function SiteFooter() {
                 target="_blank">
                     Chevy Ray
                 </Link>
-                &nbsp;
+                &nbsp;|&nbsp;
                 <Link
                 href="https://raw.githubusercontent.com/ChevyRay/pixel_font_megapack_license/main/LICENSE.txt"
                 target="_blank">
